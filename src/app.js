cc5e1f256f2dd6149b93a8aa1c013dc7ba62e49d
azam-12 <a.tamboli@hotmail.com>
2024-09-25 21:48:05 +0530

    playing with routes original

--- src/app.js
+++ src/app.js
@@ -2,30 +2,41 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/a(bc)+d", (req, res) => {
+    res.send({firstname: "Azam", lastName: "Tamboli"});
+});
+
+app.get("/ab*cd", (req, res) => {
+    res.send({firstname: "Azam", lastName: "Tamboli"});
+});
+
+app.get("/ab+c", (req, res) => {
     res.send({firstname: "Azam", lastName: "Tamboli"});
 });
 
 
-app.post("/user", (req, res) => {
-    // creates a new record in the db
-    res.send("New user created and saved to database!");
+app.get("/ab?c", (req, res) => {
+    res.send({firstname: "Azam", lastName: "Tamboli"});
 });
 
-
-app.patch("/user", (req, res) => {
-    // updates data without replacing existing record
-    res.send("Data updated successfully without replacing existing record!");
+app.get(/a/, (req, res) => {
+    res.send({firstname: "Azam", lastName: "Tamboli"});
 });
 
-app.put("/user", (req, res) => {
-    // updated data to db by replacing existing record
-    res.send("Updated data to db by replacing existing record!");
+app.get(/.*fly$/, (req, res) => {
+    res.send({firstname: "Azam", lastName: "Tamboli"});
 });
 
-app.delete("/user", (req, res) => {
-    res.send("User data deleted successfully!");
+app.get("/user?userId=101&password=testing", (req, res) => {
+    console.log(req.body);
+    res.send({firstname: "Azam", lastName: "Tamboli"});
 });
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+    res.send({firstname: "Azam", lastName: "Tamboli"});
+});
+
 
 app.listen(7777, () => {
     console.log("Sever is successfully listening on port 7777...");
